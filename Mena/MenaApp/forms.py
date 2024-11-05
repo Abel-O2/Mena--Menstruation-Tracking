9e8fbfc641e82343b8f11cf348fda1e54c15f4d1
Abel-O2 <samuel.abrenica01@gmail.com>
2024-11-06 00:37:20 +0800

    More UI changes

--- Mena/MenaApp/forms.py
+++ Mena/MenaApp/forms.py
@@ -4,9 +4,32 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    birthdate = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    email = forms.EmailField(required=True,widget=forms.EmailInput(attrs={
+        'class': 'input-field',
+        'placeholder': 'Email Address'
+    }))
+    birthdate = forms.DateField(required=True, widget=forms.DateInput(attrs={
+        'class': 'input-field',
+        'placeholder': 'dd/mm/yyyy',
+        'type':'date'
+    }))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'input-field',
+            'placeholder': 'Password'
+    }))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'input-field',
+            'placeholder': 'Confirm Password'
+    }))
     
     class Meta:
         model = User
         fields = ["username", "email", "birthdate", "password1", "password2"]
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'input-field',
+                'placeholder': 'Username'
+            }),
+        }
