4a02dbb9d395ee24a585dd7239619bdeb7040445
InFinacity <yevrahznah@yahoo.com>
2024-11-06 13:19:31 +0800

    Symptoms added

--- Mena/MenaApp/models.py
+++ Mena/MenaApp/models.py
@@ -8,3 +8,11 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Symptoms(models.Model):
+    phase = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.phase
